92f8d2d6369d22c9cc3dab043831df287288db1d
Son Roy Almerol <github@snry.me>
2025-09-20 19:30:00 -0400

    reduce test to 1m

--- test/integration/server_test.go
+++ test/integration/server_test.go
@@ -1133,8 +1133,8 @@ func testLargeCollectionHandling(t *testing.T, client *http.Client, baseURL, bas
 	baseCalendarURL := baseURL + basePath + "/calendars/alice/shared/team/"
 
 	t.Run("CreateManyEvents", func(t *testing.T) {
-		// Create 100000000 events to simulate a large collection
-		numEvents := 100_000_000
+		// Create 1000000 events to simulate a large collection
+		numEvents := 1_000_000
 		for i := 0; i < numEvents; i++ {
 			ics := fmt.Sprintf("BEGIN:VCALENDAR\r\n"+
 				"VERSION:2.0\r\n"+
