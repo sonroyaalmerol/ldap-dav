32d229760a46507a36bbf616b82289a3977974d0
Son Roy Almerol <github@snry.me>
2025-09-25 19:42:20 -0400

    fix capability string for carddav

--- internal/dav/carddav/methods.go
+++ internal/dav/carddav/methods.go
@@ -14,7 +14,7 @@ import (
 )
 
 func (h *Handlers) GetCapabilities() string {
-	return "addressbook-access"
+	return "addressbook"
 }
 
 func (h *Handlers) HandleHead(w http.ResponseWriter, r *http.Request) {
