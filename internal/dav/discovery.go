cbfaaae9227ade8d298420184967bb3bfda49304
Son Roy Almerol <github@snry.me>
2025-11-14 13:28:45 -0500

    add cors headers to well known paths

--- internal/dav/discovery.go
+++ internal/dav/discovery.go
@@ -5,6 +5,12 @@ import (
 )
 
 func (h *Handlers) HandleWellKnown(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PROPFIND, MKCOL, MKCALENDAR, REPORT")
+	w.Header().Set("Access-Control-Allow-Headers", "Authorization, If-Match, If-None-Match, If-Modified-Since, Depth, Content-Type, Content-Range, Content-Language, Date, Content-Length, Content-Encoding")
+	w.Header().Set("Access-Control-Expose-Headers", "Dav, Content-Type, Content-Range, Content-Language, Date, Content-Length, Content-Encoding, Etag, Last-Modified")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
 	// Redirect to base path per RFC 6764
 	http.Redirect(w, r, h.basePath+"/", http.StatusPermanentRedirect)
 }
